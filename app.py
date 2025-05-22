44e58ec40120ecca514aba27d9ca9b3180af9ec1
Bouzagrt Abdelhafidh <hafidoo2005@gmail.com>
2025-05-22 18:31:13 +0100

    version 1.2

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from models import db
+from bleu_print import bp
+from flask_cors import CORS   
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coworking.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+app.register_blueprint(bp)
+
+CORS(app)  
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        db.session.execute(text('SELECT 1'))
+        db.session.commit()
+    app.run(debug=True)
